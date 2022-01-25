317ddb4ff71e5e01e583114d9de176fa00df1c8c
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-25 09:51:32 +0100

    update Extras 1&2

--- exercises_practicepython.org/2_Odd_Or_Even_lvl_1.py
+++ exercises_practicepython.org/2_Odd_Or_Even_lvl_1.py
@@ -1,9 +1,16 @@
 # Ask the user for a number. 
 # Depending on whether the number is even or odd, print out an appropriate message to the user.
 
-number = int(input("Write number: "))
+# Extras:
 
-if number % 2 == 0:
+#     1. If the number is a multiple of 4, print out a different message.
+#     2. sk the user for two numbers: one number to check (call it num) and one number to divide by (check). 
+#     If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+num = int(input("Write number: "))
+check = int(input('Write number to divide: '))
+
+if num % 2 == 0:
     print('Even number!')
-else:
-    print('Odd number!')
+    if num % 4 == 0: # Extras_1 multiple of 4
+        print('Mltiple of 4')
