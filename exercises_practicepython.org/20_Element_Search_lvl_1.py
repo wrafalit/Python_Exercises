b907b61ddec0739974c31e0dfdfca967f220d263
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-25 14:21:41 +0100

    add bianry_search func

--- exercises_practicepython.org/20_Element_Search_lvl_1.py
+++ exercises_practicepython.org/20_Element_Search_lvl_1.py
@@ -5,12 +5,36 @@
 # Use binary search.
 
 def element_search(my_list,num):
-     for i in range(len(my_list)):
-         if (my_list[i]) == num:
-             return True
-         else:
-             return False
+    for i in range(len(my_list)):
+        #print(i)
+        if (my_list[i]) == num:
+            #print(my_list[i])
+            return True
+    else:
+        return False
+
+# Extras Binary Search
+def binary_search(my_list,num):
+    middle_index = int
+    while (len(my_list))>0:
+        middle_index = int((len(my_list)-1)/2)
+        # print('middle_index',middle_index)
+        # print('len(my_list)',len(my_list))
+        # print(my_list[0:middle_index+1])
+        # print(my_list[middle_index])
+        if my_list[middle_index] == num:
+            return True
+        elif my_list[middle_index] > num:
+            my_list = my_list[0:middle_index]
+            # print('L',my_list)
+        else:
+            my_list = my_list[middle_index+1:]
+            # print('P',my_list)
+    return False
+        
+                
     
 a = [5, 10, 15, 20, 25]
-num = 10
+num = 20
 print(element_search(a,num))
+print(binary_search(a,num))
