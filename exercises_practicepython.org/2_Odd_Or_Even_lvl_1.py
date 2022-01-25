6b70d8243b5080138090a64ab1ea3e7b4579711b
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-25 09:19:43 +0100

    Update 2_Odd_Or_Even_lvl_1.py

--- exercises_practicepython.org/2_Odd_Or_Even_lvl_1.py
+++ exercises_practicepython.org/2_Odd_Or_Even_lvl_1.py
@@ -1,2 +1,9 @@
 # Ask the user for a number. 
 # Depending on whether the number is even or odd, print out an appropriate message to the user.
+
+number = int(input("Write number: "))
+
+if number % 2 == 0:
+    print('Even number!')
+else:
+    print('Odd number!')
