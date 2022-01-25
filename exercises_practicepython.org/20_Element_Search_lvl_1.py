6eacd3d1e1d2862706df55d0fd2abe6049a76d89
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-25 11:52:37 +0100

    Create 20_Element_Search_lvl_1.py

---
+++ exercises_practicepython.org/20_Element_Search_lvl_1.py
@@ -0,0 +1,5 @@
+# Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number. 
+# The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+
+# Extras:
+# Use binary search.
