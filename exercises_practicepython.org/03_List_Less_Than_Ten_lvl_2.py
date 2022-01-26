c6df20febd3313833c3df61ffaf3b92200677e64
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-26 09:13:10 +0100

    add Extras1 print new_list

---
+++ exercises_practicepython.org/03_List_Less_Than_Ten_lvl_2.py
@@ -0,0 +1,22 @@
+# List Less Than Ten
+
+# Take a list, say for example this one: a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+# Extras:
+# 1. Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# 2. Write this in one line of Python.
+# 3. Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+def list_less(n1,n2):
+    new_list =[]
+    for i in n1:
+        if i < n2:
+            #print(i)
+            new_list.append(n1[i])
+    print(new_list)
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = 5
+  
+list_less(a,b)
