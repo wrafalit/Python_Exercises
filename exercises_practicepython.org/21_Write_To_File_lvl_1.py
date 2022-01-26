c2fd5b87c6cef9a36ca6253f9c6cf58e5d1c3fce
wrafalit@gmail.com <Programowanie2020#>
2022-01-26 19:05:14 +0100

    Update write solution

--- exercises_practicepython.org/21_Write_To_File_lvl_1.py
+++ exercises_practicepython.org/21_Write_To_File_lvl_1.py
@@ -2,3 +2,20 @@
 
 # Extras:
 # Ask the user to specify the name of the output file that will be saved.
+
+def max_three(a,b,c):
+    if a > b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+
+a = 50
+b = 10
+c = 14
+#print(max_three(a,b,c))
+
+file_name = str(input('Write file name to save with: '))
+with open(file_name+'.txt', 'w') as open_file:
+    open_file.write(str(max_three(a,b,c)))
