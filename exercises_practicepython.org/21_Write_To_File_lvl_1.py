1a4e6dce3ed5c888ff3890548a35b28648eb6b09
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-26 07:58:11 +0100

    to solve !

---
+++ exercises_practicepython.org/21_Write_To_File_lvl_1.py
@@ -0,0 +1,4 @@
+# write the results to a txt file. In your code, just make up a name for the file you are saving to.
+
+# Extras:
+# Ask the user to specify the name of the output file that will be saved.
