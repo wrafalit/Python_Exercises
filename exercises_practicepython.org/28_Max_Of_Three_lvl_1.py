31be15b4beb17ed445b1080f435a6c23b4322dec
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-26 08:20:07 +0100

    solved problem

--- exercises_practicepython.org/28_Max_Of_Three_lvl_1.py
+++ exercises_practicepython.org/28_Max_Of_Three_lvl_1.py
@@ -2,3 +2,16 @@
 # Do this without using the Python max() function!
 # The goal of this exercise is to think about some internals that Python normally takes care of for us. 
 # All you need is some variables and if statements!
+
+def max_three(a,b,c):
+    if a > b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+
+a = 50
+b = 10
+c = 14
+print(max_three(a,b,c))
