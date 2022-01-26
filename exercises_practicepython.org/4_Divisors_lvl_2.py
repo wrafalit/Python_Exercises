e65c3f8942e72c58cf621d652eb042d3fc359318
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-26 11:27:31 +0100

    Update comprehension code

--- exercises_practicepython.org/4_Divisors_lvl_2.py
+++ exercises_practicepython.org/4_Divisors_lvl_2.py
@@ -4,3 +4,15 @@
 # (If you don’t know what a divisor is, it is a number that divides evenly into another number. 
                                                                                                                  
 # For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+# func code
+def divisors(num):
+    for n in range(1,num+1):
+        if num % n == 0:
+            print(n)
+            
+num = int(input('Write number to print a list of all the divisors of that number: '))
+
+# code in one line
+print( [n for n in range(1,num+1) if num % n == 0])
+divisors(num)
