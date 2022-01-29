f23529ec558050a93575882ecdf4ed1fa56f92f3
wrafalit@gmail.com <Programowanie2020#>
2022-01-29 19:20:03 +0100

    solved read

--- exercises_practicepython.org/22_Read_From_File_lvl_1.py
+++ exercises_practicepython.org/22_Read_From_File_lvl_1.py
@@ -8,3 +8,29 @@
 # and lists the file directory hierarchy for the images. 
 # Once you take a look at the first line or two of the file, it will be clear which part represents the scene category. 
 # To do this, you’re going to have to remember a bit about string parsing in Python 3. I talked a little bit about it in this post.
+
+with open('nameslist.txt','r') as open_file:
+    name_file = open_file.read()
+
+def count_name(names):
+    list_name = names.split()
+    return len(names)
+
+print(count_name(name_file))
+
+with open('Training_01.txt','r') as open_file2:
+    categ_file = open_file2.read()
+
+def categ_count(file_link):
+    cate_lis = file_link.split()
+    count_cate = []
+    for n in range(len(cate_lis)):
+        # print(cate_lis[n].split('/')[2])
+        if cate_lis[n].split('/')[2] not in count_cate:
+            count_cate.append(cate_lis[n].split('/')[2])
+    print(len(count_cate))
+
+
+
+
+categ_count(categ_file)
