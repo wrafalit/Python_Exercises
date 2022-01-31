61bf4d0e1900f65da010251973049f615439cb9c
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-31 14:14:41 +0100

    solved

--- pynative.com/python-list/E3_Turn_item_list_into_square.py
+++ pynative.com/python-list/E3_Turn_item_list_into_square.py
@@ -8,3 +8,12 @@
 
 # Expected output:
 # [1, 4, 9, 16, 25, 36, 49]
+
+# Comprehaniob list
+print([x**2 for x in numbers])
+
+# function
+sq_list = []
+for n in numbers:
+    sq_list.append(n**2)
+print(sq_list)
