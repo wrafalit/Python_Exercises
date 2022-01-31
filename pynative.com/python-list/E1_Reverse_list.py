181781c507241f1835971e4d63cd181da2ec9e5d
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-31 10:43:30 +0100

    solved 2

--- pynative.com/python-list/E1_Reverse_list.py
+++ pynative.com/python-list/E1_Reverse_list.py
@@ -2,3 +2,16 @@
 
 # Given: list1 = [100, 200, 300, 400, 500]
 # Expected output: [500, 400, 300, 200, 100]
+
+# method used
+list1 = [100, 200, 300, 400, 500]
+print(list(reversed(list1)))
+
+# function used
+print(list1[-1])
+def rever(lis):
+    rev_list = []
+    for i in lis:
+        rev_list.insert(0,i)
+    return rev_list
+print(rever(list1))
