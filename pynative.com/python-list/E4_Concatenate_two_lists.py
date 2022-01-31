25b34cffaa40b10f8d36756c602ce9daec4676de
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-31 14:16:09 +0100

    Create E4_Concatenate_two_lists.py

---
+++ pynative.com/python-list/E4_Concatenate_two_lists.py
@@ -0,0 +1,8 @@
+# Exercise 4: 
+# Concatenate two lists in the following order
+
+# list1 = ["Hello ", "take "]
+# list2 = ["Dear", "Sir"]
+
+# Expected output:
+# ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
