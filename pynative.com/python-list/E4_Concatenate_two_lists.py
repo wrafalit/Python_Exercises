09eef7606257595438b6dbe1fdb1c371d99c094f
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-01 09:05:21 +0100

    solved problem

--- pynative.com/python-list/E4_Concatenate_two_lists.py
+++ pynative.com/python-list/E4_Concatenate_two_lists.py
@@ -6,3 +6,9 @@
 
 # Expected output:
 # ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+
+res = [x + y for x in list1 for y in list2]
+print(res)
