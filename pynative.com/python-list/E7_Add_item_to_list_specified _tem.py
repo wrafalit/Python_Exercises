99f7efd9026803c89f639df90c2b715ac8ce380a
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-01 10:09:03 +0100

    solved

--- pynative.com/python-list/E7_Add_item_to_list_specified _tem.py
+++ pynative.com/python-list/E7_Add_item_to_list_specified _tem.py
@@ -1,11 +1,15 @@
-Exercise 7: Add new item to list after a specified item
+# Exercise 7: 
+# Add new item to list after a specified item
 
-Write a program to add item 7000 after 6000 in the following Python List
+# Write a program to add item 7000 after 6000 in the following Python List
 
-Given:
+# Given:
+# list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+# Expected output:
+# [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
 
 list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-
-Expected output:
-
-[10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
+print(list1[2][2])
+list1[2][2].append(700)
+print(list1)
