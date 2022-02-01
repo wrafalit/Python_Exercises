1b8b6d1b07c363ef90c9ca98741591c42662ae64
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-01 12:32:20 +0100

    Create E8_Extend_nested_list_adding_sublist.py

---
+++ pynative.com/python-list/E8_Extend_nested_list_adding_sublist.py
@@ -0,0 +1,13 @@
+# Exercise 8: 
+# Extend nested list by adding the sublist
+
+# You have given a nested list. Write a program to extend it by adding the sublist ["h", "i", "j"] in such a way that it will look like the following list.
+
+# Given List:
+# list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+
+# # sub list to add
+# sub_list = ["h", "i", "j"]
+
+# Expected Output:
+# ['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
