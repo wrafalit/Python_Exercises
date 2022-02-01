9e8984ca69804af1ef6803ef4681c131d25d01aa
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-01 12:34:42 +0100

    solved

--- pynative.com/python-list/E8_Extend_nested_list_adding_sublist.py
+++ pynative.com/python-list/E8_Extend_nested_list_adding_sublist.py
@@ -11,3 +11,12 @@
 
 # Expected Output:
 # ['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
+
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+
+# sub list to add
+sub_list = ["h", "i", "j"]
+
+print(list1[2][1][2])
+list1[2][1][2].extend(sub_list)
+print(list1)
