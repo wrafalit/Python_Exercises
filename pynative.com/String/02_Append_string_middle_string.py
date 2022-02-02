76b149c24c83e0625cee98a7ca6451e37b605b34
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-02 09:26:56 +0100

    Create 02_Append_string_middle_string.py

---
+++ pynative.com/String/02_Append_string_middle_string.py
@@ -0,0 +1,10 @@
+# Exercise 2: 
+# Append new string in the middle of a given string
+
+# Given two strings, s1 and s2. Write a program to create a new string s3 by appending s2 in the middle of s1.
+
+# Given:
+# s1 = "Ault"
+# s2 = "Kelly"
+
+# Expected Output:  AuKellylt
