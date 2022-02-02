028efb1146227dba52076c67bcf10d0c84467a9f
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-02 09:37:29 +0100

    Create 03_String of_two_fir_mid_end.py

---
+++ pynative.com/String/03_String of_two_fir_mid_end.py
@@ -0,0 +1,10 @@
+# Exercise 3: 
+# Create a new string made of the first, middle, and last characters of each input string
+
+# Given two strings, s1 and s2, write a program to return a new string made of s1 and s2’s first, middle, and last characters.
+
+# Given:
+# s1 = "America"
+# s2 = "Japan"
+
+# Expected Output: AJrpan
