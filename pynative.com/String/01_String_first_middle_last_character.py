cc7e07455133ac3b45587bc30b965109ec522e3e
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-02 08:51:15 +0100

    Solved 1A & 1B

--- pynative.com/String/01_String_first_middle_last_character.py
+++ pynative.com/String/01_String_first_middle_last_character.py
@@ -9,3 +9,32 @@
 # Expected Output:
 # Jms
 
+str1 = "James"
+mid = int(len(str1)/2)
+print(str1[0] + str1[mid] + str1[-1])
+
+# Exercise 1B: 
+# Create a string made of the middle three characters
+
+# Write a program to create a new string made of the middle three characters of an input string.
+
+# Given:
+# Case 1
+# str1 = "JhonDipPeta"
+
+# Output: Dip
+
+# Case 2
+# str2 = "JaSonAy"
+
+# Output: Son
+
+str1 = "JhonDipPeta"
+str2 = "JaSonAy"
+
+def midle_three(str0):
+    mid = int(len(str0)/2)
+    print(str0[(mid-1):((mid+2))])
+    
+midle_three(str1)
+midle_three(str2)
