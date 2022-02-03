3a4cb7d69c0fe7a634cbaaf3f07df4c7e2d9e4bd
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-03 08:37:53 +0100

    Create 08

---
+++ pynative.com/String/08_Count_string_sentence.py
@@ -0,0 +1,8 @@
+# xercise 8: 
+#   Find all occurrences of a substring in a given string by ignoring the case
+
+# Write a program to find all occurrences of “USA” in a given string ignoring the case.
+
+# Given: str1 = "Welcome to USA. usa awesome, isn't it?"
+
+# Expected Outcome: The USA count is: 2
