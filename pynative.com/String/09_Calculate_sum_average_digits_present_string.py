8a3918aac331abd4e4b92439e03e382ae4c7746c
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-03 10:00:54 +0100

    Create

---
+++ pynative.com/String/09_Calculate_sum_average_digits_present_string.py
@@ -0,0 +1,8 @@
+Exercise 9: 
+Calculate the sum and average of the digits present in a string
+
+Given a string s1, write a program to return the sum and average of the digits that appear in the string, ignoring all other characters.
+
+Given:              str1 = "PYnative29@#8496"
+Expected Outcome:   Sum is: 38 Average is  6.333333333333333
+    
