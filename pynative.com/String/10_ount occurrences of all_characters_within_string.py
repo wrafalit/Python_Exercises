cae2792eddecbdb980e036795c22022aff0127d3
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-03 12:46:50 +0100

    add second solution

--- pynative.com/String/10_ount occurrences of all_characters_within_string.py
+++ pynative.com/String/10_ount occurrences of all_characters_within_string.py
@@ -4,3 +4,19 @@ Write a program to count occurrences of all characters within a string
 Given: str1 = "Apple"
 
 Expected Outcome: {'A': 1, 'p': 2, 'l': 1, 'e': 1}
+
+dic = {}
+for ch in range(len(str1)):
+    if str1[ch] in dic:
+        dic[str1[ch]] += 1
+    else:
+        dic[str1[ch]] = 1
+        
+print(dic)
+
+# Second solution
+
+dic = {}
+for ch in range(len(str1)):
+    dic[str1[ch]] = str1.count(str1[ch])
+print(dic)
