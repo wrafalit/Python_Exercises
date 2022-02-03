d8490a6c7ae43c4bbc4e54074db722ca016cf1a7
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-03 14:55:28 +0100

    Create 11_Reverse_string.py

---
+++ pynative.com/String/11_Reverse_string.py
@@ -0,0 +1,14 @@
+# Exercise 11: 
+# Reverse a given string
+
+# Given:  str1 = "PYnative"
+
+# Expected Output: evitanYP
+
+str1 = "PYnat ive"
+mystr = list(reversed(str1))
+print(mystr)
+resv_str = ''.join(mystr)
+print(resv_str)
+
+
