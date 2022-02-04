5655e9215bd2be4f207d65c8fc775e2d92a8ed92
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-04 09:26:36 +0100

    Create 13_Split_string_hyphens.py

---
+++ pynative.com/String/13_Split_string_hyphens.py
@@ -0,0 +1,16 @@
+# Exercise 13: 
+# Split a string on hyphens
+
+# Write a program to split a given string on hyphens and display each substring.
+
+# Given: str1 = Emma-is-a-data-scientist
+
+# Expected Output:
+
+# Displaying each substring
+
+# Emma
+# is
+# a
+# data
+# scientist
