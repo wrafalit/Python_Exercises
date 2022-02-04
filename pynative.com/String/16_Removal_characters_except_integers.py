bf1377acff476d0f4321e055f351e5d0c694f3eb
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-04 13:11:24 +0100

    2nd solution

--- pynative.com/String/16_Removal_characters_except_integers.py
+++ pynative.com/String/16_Removal_characters_except_integers.py
@@ -4,3 +4,13 @@
 # Given:             str1 = 'I am 25 years and 10 months old'
 
 # Expected Output:  2510
+
+str1 = 'I am 25 years and 10 months old'
+str2 = ''
+for ch in str1:
+    if ch.isdigit():
+        str2 += ch
+print(str2)
+
+# 2 solution
+print(''.join([x for x in str1 if x.isdigit()]))
