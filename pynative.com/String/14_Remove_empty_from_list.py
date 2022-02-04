26c69065b2ab9f19d17e6636bc712863fc9f3238
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-04 09:30:50 +0100

    Create 14_Remove_empty_from_list.py

---
+++ pynative.com/String/14_Remove_empty_from_list.py
@@ -0,0 +1,12 @@
+# Exercise 14: 
+# Remove empty strings from a list of strings
+
+# Given:  str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+
+# Expected Output:
+
+# Original list of sting
+# ['Emma', 'Jon', '', 'Kelly', None, 'Eric', '']
+
+# After removing empty strings
+# ['Emma', 'Jon', 'Kelly', 'Eric']
