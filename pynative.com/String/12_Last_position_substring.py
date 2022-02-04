befd837e5987a0def3851e40454187810e9b39fe
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-04 09:16:59 +0100

    add my function for exercise

--- pynative.com/String/12_Last_position_substring.py
+++ pynative.com/String/12_Last_position_substring.py
@@ -9,3 +9,19 @@
 
 str1 = "Emma is a data scientist who knows Python. Emma works at google."
 print("Last occurrence of Emma starts at index ",str1.rfind("Emma"))
+
+
+# My function (practice / exercise)
+
+def my_finder_string(string):
+    index = -1
+    i = 0
+    while i < len(string) - 1:
+        if "Emma" in string[i:i+4]:
+            index = i
+            i += 1
+        else:
+            i += 1
+    return index
+
+print(my_finder_string(str1))
