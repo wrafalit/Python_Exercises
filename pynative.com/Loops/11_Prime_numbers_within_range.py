0df873b2edda6d08deea7e94b84bacd351c8fbdf
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 11:25:56 +0100

    Create 11_Prime_numbers_within_range.py

---
+++ pynative.com/Loops/11_Prime_numbers_within_range.py
@@ -0,0 +1,26 @@
+# Exercise 11: 
+# Write a program to display all prime numbers within a range
+
+# Note: A Prime Number is a number that cannot be made by multiplying other whole numbers. 
+# A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers
+
+# Examples:
+
+#     6 is not a prime mumber because it can be made by 2×3 = 6
+#     37 is a prime number because no other whole numbers multiply together to make it.
+
+# Given:
+
+# # range
+# start = 25
+# end = 50
+
+# Expected output:
+
+# Prime numbers between 25 and 50 are:
+# 29
+# 31
+# 37
+# 41
+# 43
+# 47
