ec940f5475f30251c08c5ab5959eb798164291fa
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 13:09:21 +0100

    Update 14_Reverse_given_number.py

--- pynative.com/Loops/14_Reverse_given_number.py
+++ pynative.com/Loops/14_Reverse_given_number.py
@@ -1,10 +1,15 @@
 # Exercise 14: 
 # Reverse a given integer number
 
-# Given:
+# Given:            # 76542
+# Expected output:  # 24567
 
-# 76542
+num = 123456789
+print(num)
+reverse = 0
+while num > 0:
+    reminder = num % 10
+    reverse = (reverse * 10) + reminder
+    num = num // 10
 
-# Expected output:
-
-# 24567
+print(reverse)
