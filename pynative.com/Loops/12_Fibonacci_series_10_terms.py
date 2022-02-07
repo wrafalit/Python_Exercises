411f7cf3d3aff20c4fd6e2cfeeffb9f07c20e983
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 12:26:00 +0100

    Update 12_Fibonacci_series_10_terms.py

--- pynative.com/Loops/12_Fibonacci_series_10_terms.py
+++ pynative.com/Loops/12_Fibonacci_series_10_terms.py
@@ -4,3 +4,13 @@ for i in range(n+1):
     if i > 2:
         fibunaci.append(fibunaci[i-3] + fibunaci[i-2])
 print(fibunaci)
+
+
+fibunaci = [0,1]
+n = 10
+for i in range(n+1):
+    if i > 2:
+        fibunaci.append(fibunaci[i-3] + fibunaci[i-2])
+
+fibunaci = list(map(str, fibunaci))
+print(' '.join(fibunaci))
