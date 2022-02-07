cad1bfd5501e1b8c5cf037294370ae47fd8cf0d4
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 10:09:55 +0100

    solved

--- pynative.com/Loops/05_Display_numbers_from_list_loop.py
+++ pynative.com/Loops/05_Display_numbers_from_list_loop.py
@@ -13,3 +13,13 @@
 # 75
 # 150
 # 145
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for n in numbers:
+    if n > 500:
+        break
+    elif n > 150:
+        continue
+    elif n % 5 == 0:
+        print(n)
