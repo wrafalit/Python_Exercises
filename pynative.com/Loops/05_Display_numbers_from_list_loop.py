30b890ef060aa585a7b8ad162b480fc966be5e7e
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 10:09:33 +0100

    Create 05_Display_numbers_from_list_loop.py

---
+++ pynative.com/Loops/05_Display_numbers_from_list_loop.py
@@ -0,0 +1,15 @@
+# Exercise 5:
+# Display numbers from a list using loop
+
+# Write a program to display only those numbers from a list that satisfy the following conditions
+
+#     The number must be divisible by five
+#     If the number is greater than 150, then skip it and move to the next number
+#     If the number is greater than 500, then stop the loop
+
+# Given: numbers = [12, 75, 150, 180, 145, 525, 50]
+
+# Expected output: 
+# 75
+# 150
+# 145
