aa61932e4add02817a5868a22e809ec99e989285
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 10:53:03 +0100

    add  2end solution

--- pynative.com/Loops/08_List_reverse_order_loop.py
+++ pynative.com/Loops/08_List_reverse_order_loop.py
@@ -16,4 +16,9 @@
 list1 = [10, 20, 30, 40, 50]
 for n in range(len(list1)-1,-1,-1):
     print(list1[n])
-    
+
+# 2end solution
+list2 = reversed(list1)
+print(list2)
+for n in list2:
+    print(n)
