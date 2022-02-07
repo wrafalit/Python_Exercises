ae4e4a58bf11195a3780d0b79094652af432b0e9
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-07 10:24:55 +0100

    Update 06_Count_total_number_digits_number.py

--- pynative.com/Loops/06_Count_total_number_digits_number.py
+++ pynative.com/Loops/06_Count_total_number_digits_number.py
@@ -4,3 +4,11 @@
 # Write a program to count the total number of digits in a number using a while loop.
 
 # For example, the number is 75869, so the output should be 5.
+
+number = 75869
+
+count = 0
+while number != 0:
+    number = number // 10
+    count += 1
+print(count)
