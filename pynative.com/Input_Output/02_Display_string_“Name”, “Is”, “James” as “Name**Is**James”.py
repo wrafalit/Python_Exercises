905132c91ab2fcee1175b445b8d3ef84540442aa
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-09 08:01:06 +0100

    Create 02_Display_string_“Name”, “Is”, “James” as “Name**Is**James”.py

---
+++ pynative.com/Input_Output/02_Display_string_“Name”, “Is”, “James” as “Name**Is**James”.py
@@ -0,0 +1,10 @@
+xercise 2: 
+Display three string “Name”, “Is”, “James” as “Name**Is**James”
+
+Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+
+Expected Output:
+
+For example: print('Name', 'Is', 'James') will display Name**Is**James
+  
+print('My', 'Name', 'Is', 'James', sep='**')
