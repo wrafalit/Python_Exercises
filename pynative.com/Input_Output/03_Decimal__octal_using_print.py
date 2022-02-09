4833651ea50888ffa55e0f548296c03834203a40
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-09 08:07:37 +0100

    Create 03_Decimal__octal_using_print.py

---
+++ pynative.com/Input_Output/03_Decimal__octal_using_print.py
@@ -0,0 +1,13 @@
+# Exercise 3: 
+# Convert Decimal number to octal using print() output formatting
+
+# Given:
+
+# num = 8
+
+# Expected Output:
+
+# The octal number of decimal number 8 is 10
+
+num = 8
+print("{0:o}".format(num))
