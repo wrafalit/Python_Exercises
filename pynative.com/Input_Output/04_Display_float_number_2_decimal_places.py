aea325c473ff56238a6cd5a0b80923533268400c
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-09 08:32:29 +0100

    Create 04_Display_float_number_2_decimal_places.py

---
+++ pynative.com/Input_Output/04_Display_float_number_2_decimal_places.py
@@ -0,0 +1,13 @@
+# Exercise 4: 
+# Display float number with 2 decimal places using print()
+
+# Given:
+
+# num = 458.541315
+
+# Expected Output:
+
+# 458.54
+
+num = 458.541315
+print('%.2f' % num)
