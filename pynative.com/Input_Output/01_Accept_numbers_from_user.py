7529dfcae0eb78705a4c84a5e9efaca6f9cd08ac
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-09 07:50:02 +0100

    Create 01_Accept_numbers_from_user.py

---
+++ pynative.com/Input_Output/01_Accept_numbers_from_user.py
@@ -0,0 +1,10 @@
+# Exercise 1: 
+# Accept numbers from a user
+
+# Write a program to accept two numbers from the user and calculate multiplication
+
+# Help: Take user input in Python
+
+number1 = int(input('Write number1: '))
+number2 = int(input('Write number2: '))
+print("Multiplication is ", number1 * number2)
