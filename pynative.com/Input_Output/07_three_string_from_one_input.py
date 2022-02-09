3fd77436c493c87a1f530fb38ec776decec18144
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-09 08:49:23 +0100

    Create 07_three_string_from_one_input.py

---
+++ pynative.com/Input_Output/07_three_string_from_one_input.py
@@ -0,0 +1,10 @@
+# Exercise 7: 
+# Accept any three string from one input() call
+
+# Write a program to take three names as input from a user in the single input() function call.
+
+name1, name2, name3 = input("Enter three string ").split()
+
+print("Name1: ", name1)
+print("Name2: ", name2)
+print("Name3: ", name3)
