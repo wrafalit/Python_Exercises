424ad0825a9bcb0f375fffa2fc6bc6be0141c41d
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-09 09:13:01 +0100

    Create 08_Format_variables_using_string.format_method.py

---
+++ pynative.com/Input_Output/08_Format_variables_using_string.format_method.py
@@ -0,0 +1,20 @@
+Exercise 8: 
+Format variables using a string.format() method.
+
+Write a program to use string.format() method to format the following three variables as per the expected output
+
+Given:
+
+totalMoney = 1000
+quantity = 3
+price = 450
+
+Expected Output:
+
+I have 1000 dollars so I can buy 3 football for 450.00 dollars.
+
+totalMoney = 1000
+quantity = 3
+price = 450
+statement1 = "I have {1} dollars so I can buy {0} football for {2:.2f} dollars."
+print(statement1.format(totalMoney,quantity,price))
