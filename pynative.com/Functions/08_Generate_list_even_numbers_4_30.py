ee6f47e5ebdbec6867cd80486839e20c08c5630e
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-10 14:31:50 +0100

    Create 08_Generate_list_even_numbers_4_30.py

---
+++ pynative.com/Functions/08_Generate_list_even_numbers_4_30.py
@@ -0,0 +1,8 @@
+# Exercise 8: 
+# Generate a Python list of all the even numbers between 4 to 30
+
+# Expected Output:
+
+# [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+
+print(list(range(4, 32, 2)))
