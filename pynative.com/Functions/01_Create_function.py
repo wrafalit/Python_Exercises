9314a35f0843bf7c6cce8ee7a7c9cea92f4799c3
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-10 11:27:03 +0100

    Create 01_Create_function.py

---
+++ pynative.com/Functions/01_Create_function.py
@@ -0,0 +1,9 @@
+# Exercise 1: 
+# Create a function in Python
+
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def func(name, age):
+    print(name, age)
+    
+func("Ben", 25)
