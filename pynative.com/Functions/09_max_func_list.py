10e9bfacead640cce136a9e2d42d13da4c2d0949
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-10 14:34:13 +0100

    Create 09_max_func_list.py

---
+++ pynative.com/Functions/09_max_func_list.py
@@ -0,0 +1,14 @@
+# Exercise 9: 
+# Find the largest item from a given list
+
+# x = [4, 6, 8, 24, 12, 2]
+
+# Expected Output:
+
+# 24
+
+x = [4, 6, 8, 24, 12, 2]
+max_x = max(x)
+ind_max = x.index(24)
+print(max_x)
+print(ind_max)
