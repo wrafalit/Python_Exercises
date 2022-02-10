ebb9d9846225e709c42fc06faf1ce0d80cd012b9
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-10 14:29:33 +0100

    Create 07_Assign_different_name_function_and_call_through_new_name.py

---
+++ pynative.com/Functions/07_Assign_different_name_function_and_call_through_new_name.py
@@ -0,0 +1,23 @@
+# Exercise 7: 
+# Assign a different name to function and call it through the new name
+
+# Below is the function display_student(name, age). Assign a new name show_tudent(name, age) to it and call it using the new name.
+
+# Given:
+
+# def display_student(name, age):
+#     print(name, age)
+
+# display_student("Emma", 26)
+
+# You should be able to call the same function using
+
+# show_student(name, age)
+
+def display_student(name, age):
+    print(name, age)
+
+display_student("Emma", 26)
+
+show_student = display_student
+show_student("Emma", 26)
