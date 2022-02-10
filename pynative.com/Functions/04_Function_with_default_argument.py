f3ed600b119eaac50e52200848d47ccf26d5dc40
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-10 12:06:30 +0100

    Create 04_Function_with_default_argument.py

---
+++ pynative.com/Functions/04_Function_with_default_argument.py
@@ -0,0 +1,26 @@
+# Exercise 4: 
+# Create a function with default argument
+
+# Write a program to create a function show_employee() using the following conditions.
+
+#     It should accept the employee’s name and salary and display both.
+#     If the salary is missing in the function call then assign default value 9000 to salary
+
+# See: Default arguments in function
+
+# Given:
+
+# showEmployee("Ben", 12000)
+# showEmployee("Jessa")
+
+# Expected output:
+
+# Name: Ben salary: 12000
+# Name: Jessa salary: 9000
+
+def showEmployee(name, salary = 9000):
+    print(f"Name: {name} salary: {salary}")
+    
+    
+showEmployee("Ben", 12000)
+showEmployee("Jessa")
