a2294a0ca4dfd9c303ac2030c3d87e4dc26a6dd2
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-11 11:13:26 +0100

    Create 01_Picking_odd-index_first_list_even_index_items_from_second.py

---
+++ pynative.com/Data_Structure/01_Picking_odd-index_first_list_even_index_items_from_second.py
@@ -0,0 +1,19 @@
+xercise 1: 
+Create a list by picking an odd-index items from the first list and even index items from the second
+
+Given two lists, l1 and l2, write a program to create a third list l3 by picking an odd-index element from the list l1 and even index elements from the list l2.
+
+Given:
+
+l1 = [3, 6, 9, 12, 15, 18, 21]
+l2 = [4, 8, 12, 16, 20, 24, 28]
+
+Expected Output:
+
+Element at odd-index positions from list one
+[6, 12, 18]
+Element at even-index positions from list two
+[4, 12, 20, 28]
+
+Printing Final third list
+[6, 12, 18, 4, 12, 20, 28]
