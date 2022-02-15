f8ab8cad36ac6ce759181c9bd6a9225f3f9014dc
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-02-15 11:57:20 +0100

    Update 01_Create_Class_with_instance_attributes.py

--- pynative.com/OOP/01_Create_Class_with_instance_attributes.py
+++ pynative.com/OOP/01_Create_Class_with_instance_attributes.py
@@ -1,8 +1,3 @@
-# OOP Exercise 1: Create a Class with instance attributes
-
-# Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
-
-
 class Vehicle:
     
     def __init__(self, max_speed, mileage):
@@ -11,4 +6,4 @@ class Vehicle:
         
 
 modelX = Vehicle(250,20_000)
-print(modelX.max_speed, mileage)
+print(modelX.max_speed, modelX.mileage)
